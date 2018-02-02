ff5cbce7009c507fea4772438ce752e5f5a3712f
mks9900 <johan@birkagatan.com>
2018-02-02 14:41:40 +0100

    Fix a few problems.

--- start_lightroom_wine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wine_folder=/home/johan/.wine/drive_c
-lr_folder=Program\ Files/Adobe/Adobe\ Photoshop\ Lightroom\ 5.7
-lr_exec=lightroom.exe
-wine_exec=/usr/bin/wine
-#exec lightroom
-$wine_exec "$wine_folder"/"$lr_folder"/$lr_exec

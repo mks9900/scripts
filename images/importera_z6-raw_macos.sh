59c9fc8f8086aa90aae3fe5a9b5ef1c67e3d85da
Johan Thor <johan.thor@sodra.com>
2021-02-08 16:56:51 +0100

    Updates for macOS

--- images/importera_z6-raw_macos.sh
+++ images/importera_z6-raw_macos.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
-Source1=/media/johan/"NIKON Z 6"/DCIM/
-Source2="100NCZ_6"
-Source="$Source1""$Source2"
-Target=$HOME/Pictures/Fotografier/import-z6-raw
-PhotoPath=$HOME/Pictures/Fotografier
+Source=/Volumes/"NIKON Z 6"/DCIM/100NCZ_6
+Target=/Volumes/Raw_photos/raws/imports
+PhotoPath=/Volumes/Raw_photos/raws/"Lightroom CC"/02fbfd9b03384e0e80642b882c22a79b/originals/
 
 clear
 
@@ -15,75 +13,78 @@ if [ ! -d "$Source" ]; then
     echo "==================================================================================="
     echo "Katalogen" $Source "finns ej, förmodligen är kortet ej monterat. Avbryter."
     echo "==================================================================================="
-    exit ; 
+    exit ;
 else
-    
+
     if [ ! -d "$Target" ]; then
 	echo "==================================================================================="
 	echo "Katalogen" $Target "finns ej, förmodligen är disken ej monterad. Avbryter."
 	echo "==================================================================================="
-	exit ; 
+	exit ;
     else
-	
+
 # TODO: Felhantering om det redan finns kataloger/filer i import som är samma som på kortet!
-	
+
 	echo
 	echo "==================================================================================="
 	echo "Flyttar alla råfiler till målet:" #samt att vi kollar vad tiden är så vi kan räkna ut hur lång tid allt tog i slutet:
 	echo "==================================================================================="
 
-	Amount="`ls -s --block-size=M "$Source"|awk '{print $2}'|head -1|sed -e 's/M//'`"
+	Amount="$(du -chsm $Source|awk '{print $1}'|head -1|sed -e 's/M//'|sed -e "s/\(\.[0-9]\).*/\1/g")"
+  #Amount="$(echo "$Amount2"|sed -e "s/\(\.[0-9]\).*/\1/g")"
+
 	Tstart="$(date +%s)"
 
 	cp -av "$Source"/*.NEF "$Target"
-	echo "Nu är alla filer kopierade!"
+	echo "\nNu är alla filer kopierade från minneskortet till flyttbara disken."
 
+	#Felhantering om Ttrans=0!...
 	Ttrans="$(($(date +%s)-Tstart))"
-#Felhantering om Ttrans=0!
-	
+
 	cd "$Target"
 
-	Count=`ls -1 *.NEF 2>/dev/null | wc -l`
+	Count="$(ls -1 *.NEF 2>/dev/null | wc -l)"
+
 	echo
-	echo "Det finns $Count NEF-filer"
+	echo "Det finns $Count flyttade NEF-filer."
 	echo
 	echo
 	echo "==================================================================================="
-	echo "Sedan byts namnet på dem och filerna flyttas till rätt datumkatalog..."
+	echo "Döper om varje fil till något mer meningsfullt:"
 	echo "==================================================================================="
 
 # Felhantering för *.NEF finns nedan:
 
 	if [ $Count = 0 ]
-	then 
+	then
 	    echo "Inga filer finns, avbryter!"
 	    exit; else
 
 	    Counter=1
 	    for RawFile in "$Target"/*.NEF; do
-
+        # OBS! exiv2 ändrar format beroende på vilket språk terminalen har...
+        # Man kan alltså få ändra $x nedan:
 		FileDate="`exiv2 "$RawFile"|grep timestamp|awk '{print $4}'|sed -e 's/:/-/g'`"
 		FileTime="`exiv2 "$RawFile"|grep timestamp|awk '{print $5}'|sed -e 's/:/-/g'`" #|cut -c1-5`" #skär bort sekundrarna...
-		FocalLength="`exiv2 "$RawFile"|grep Focal|awk '{print $4}'|cut -d'.' -f1`"
-		ISOSpeed="`exiv2 "$RawFile"|grep "ISO speed"|awk '{print $4}'`"
+		FocalLength="`exiv2 "$RawFile"|grep length|awk '{print $4}'|cut -d'.' -f1`"
+		ISOSpeed="`exiv2 "$RawFile"|grep "ISO"|awk '{print $4}'`"
 		Aperture="`exiv2 "$RawFile"|grep Aperture|awk '{print $3}'|grep F|sed -e s'/\./-/'|sed -e s'/F/f/'`" #Byt ut punkten mot _
 #		LensMaker="`exiv2 -pa "$RawFile"|grep "LensIDNumber"|awk '{print $4}'`"
 #		LensID="`exiv2 -pa "$RawFile"|grep "Exif.NikonLd3.LensIDNumber"|awk '{print $5"_"$6"-"$7}'`"
-		CameraMaker="`exiv2 "$RawFile"|grep "Camera model"|awk '{print $4}'|sed -e 's/:/-/g'`"
-		CameraModel="`exiv2 "$RawFile"|grep "Camera model"|awk '{print $5}'|sed -e 's/:/-/g'`"
+		#CameraMaker="`exiv2 "$RawFile"|grep "make"|awk '{print $3}'|sed -e 's/:/-/g'`"
+		CameraModel="`exiv2 "$RawFile"|grep "model"|awk '{print $4,$5$6}'|sed -e 's/ /_/g'`"
 #		FileName=$FileDate"_kl-"$FileTime"_"$LensMaker"_"$FocalLength""mm_ISO$ISOSpeed"_"$Aperture
 		FileName=$FileDate"_kl"$FileTime"_"$CameraModel"_"$FocalLength"mm_"$Aperture"_ISO""$ISOSpeed"
 
 		if [ ! -d "$FileDate" ]; then
 		    Counter=1
 		    mkdir -p "$FileDate"
-		    echo "Flyttar $RawFile till katalogen $FileDate."
 		    mv -v "$RawFile" "$FileDate"/"$FileName""_"$Counter.nef
 		    Counter=$[Counter + 1]
 		else
 		    mv -v "$RawFile" "$FileDate"/"$FileName""_"$Counter.nef
 		    echo "$FileDate"/"$FileName""_"$Counter.nef
-		    Counter=$[Counter + 1]		
+		    Counter=$[Counter + 1]
 		fi
 
 	    done
@@ -102,29 +103,33 @@ echo "==========================================================================
 # Kan även denna slås samman med den större for-loopen ovan?
 
 for TempDir in *; do
+    echo $TempDir
     CurrentDirectory=$(pwd)
-#    echo $pwd
-    Year=`ls |sort|awk '{print $1}'|cut -c1-4|head -1`
+    Year="$(ls |sort|awk '{print $1}'|cut -c1-4|head -1)"
     if [ ! -d "$PhotoPath"/"$Year"/"$TempDir" ]; then
-	echo "Katalogen $PhotoPath/$Year/$TempDir finns ej: Flyttar allt."
+	echo "Katalogen finns ej: Flyttar hela katalogen..."
 	mv -vi "$TempDir"/ "$PhotoPath"/"$Year"
 	echo
     else
-	echo "Katalogen $PhotoPath/$Year/$TempDir finns: Kopierar..."
+	echo "Katalogen finns: Kopierar filerna till katalogen..."
 	cp -ai "$TempDir"/*.nef "$PhotoPath"/"$Year"/"$TempDir"
+	echo "Städa manuellt undan bilderna i $TempDir!"
 	echo
+
     fi
 done
 
 # Här kollas den sista tiden och subtraherar den första så att totalen fås:
 Ttotal="$(($(date +%s)-Tstart))"
 
-TransferRate=`echo "$Amount / $Ttrans"|bc -l | sed -e "s/\(\.[0-9]\).*/\1/g"`
+TransferRate="$(echo $Amount / $Ttrans|bc -l)"
 
 echo
 echo
 echo "==================================================================================="
-echo -e "Överförde $Count filer från "$Source" om $Amount Mbyte på $Ttrans sekunder \n($TransferRate Mbyte per sekund) till $Target."
+echo -e "Överförde $Count filer från om $Amount Mbyte på $Ttrans sekunder."
+echo -e "\n($TransferRate Mbyte per sekund)."
+#echo -e "Överförde filerna från "$Source" till $Target."
 echo
 echo "Hela importen tog $Ttotal sekunder."
 echo "==================================================================================="

5954fba08f4dd119ee1e2a068154fc6e5e5b2145
Johan Thor <johan.thor@sodra.com>
2021-04-07 09:00:52 +0200

    Fix for gitignore

--- test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-select svar in  Release Relwithdebinfo Debug; do
-    case $svar in
-        Release ) buildType=$svar;echo "Du valde $buildType. "; break;;
-        Relwithdebinfo ) buildType=$svar; echo "Du valde $buildType"; break;;
-	Debug ) buildType=$svar; echo "Du valde $buildType"; break;;
-    esac
-done

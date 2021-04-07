cd89c09513dd76021f411e16c45771c18926691e
Johan Thor <johan.thor@sodra.com>
2021-04-07 08:56:55 +0200

    Modified images backup-script to zsh.

--- backup/backup_bilder_macos.sh
+++ backup/backup_bilder_macos.sh
@@ -1,85 +1,30 @@
-#!/bin/bash
+#!/bin/zsh
 #
 # progress-feedback? Via "uxterm -e 'kommando'"?
 #
-# Synkar från /home/johan/Bilder till NAS och usb-diskarna.
+# Synkar från /home/johan/Bilder till NAS.
 # --delete tar bort filer vid målet som ej finns i källan.
 #
 
-SourceBase=$HOME
-AllPath=Pictures
-YearPath="$AllPath/Fotografier"
-DevSource="//ng-nas/backup"
-TRY="`cat /etc/mtab |grep backup_nas|awk '{print $1}'|grep ng-nas`"
-NgNasBase=/home/johan/backup_nas
-OneTB_ntfsBase=/media/johan/OneTB_ntfs/backup
+Source=/Volumes/Raw_photos/raws/"Lightroom CC"/02fbfd9b03384e0e80642b882c22a79b/originals/2005/
 
+Target="/Volumes/backup/Pictures/Raw_photos/2005"
 
+mount_test="/Volumes/Raw_photos"
 
-Source="/Volumes/Raw_photos/raws/Lightroom CC/02fbfd9b03384e0e80642b882c22a79b/originals"
-SourceExtDisk="/Volumes/backup/Pictures/Raw"
-NgNas="$NgNasBase"
+if [ -d "$mount_test" ]; then
+    echo "=========================================================="
+    echo "Speglar till ng-nas:"
+    echo "=========================================================="
+    /usr/bin/rsync -hlrtv --exclude 'lost+found' --exclude '.Trash-1000' --exclude '.DS_Store' --delete $Source $Target
+    Nas_Success=1;
+else
+    echo "Målenheten är inte monterad!"
+    Nas_Success=0    
 
-
-echo "Till vilken enhet vill du säkerhetskopiera?"
-select enhet in Alla NAS; do # onedrive; do 
-    case $enhet in
-	Alla ) echo "Speglar till NAS, OneTB_ntfs, ExtDiskTwo";
-	       # Synka först till ng-nas om den är monterad, annars avbryt:
-	       if [ "$DevSource" = "$TRY" ]; then
-		   echo "Säkerhetskopierar till alla backup-enheter.";
-		   echo "=========================================================="
-		   echo "Speglar till ng-nas:"
-		   echo "=========================================================="
-
-		   /usr/bin/rsync -hlrtvz --exclude 'lost+found' --exclude '.Trash-1000' --exclude 'Kamerabilder' --delete $Source $NgNas
-		   Nas_Success=1; 
-	       else
-		   echo "Enheten $DevSource är ej monterad på $Source. Avbryter."
-		   Nas_Success=0
-	       fi
-
-	       break;;
-	
-	NAS ) echo "Speglar till NAS:en.";
-	      # Synka till ng-nas om den är monterad, annars avbryt:
-	      if [ "$DevSource" = "$TRY" ]; then
-		  echo "=========================================================="
-		  echo "Speglar till ng-nas:"
-		  echo "=========================================================="
-		  /usr/bin/rsync -hlrtvz --exclude 'lost+found' --exclude '.Trash-1000' --exclude 'Kamerabilder' --delete $Source $NgNas
-#		  echo $Source/
-#		  echo $NgNas/
-		  Nas_Success=1; 
-	      else
-		  echo "Enheten $DevSource är ej monterad på $Source. Avbryter."
-		  Nas_Success=0;
-	      fi
-	      break;;
-	
-	# OneTB_ntfs ) echo "Speglar till OneTB_ntfs.";
-	# 	  # Synka till OneTB_ntfs om den är monterad, annars avbryt:
-	# 	  if [ -d "$OneTB_ntfs" ]; then
-	# 	      echo "=========================================================="
-	# 	      echo "Speglar till OneTB_ntfs:"
-	# 	      echo "=========================================================="
-	# 	      /usr/bin/rsync -hlrtvz --exclude 'lost+found' --exclude '.Trash-1000' --exclude 'Kamerabilder' --delete $Source "$OneTB_ntfs"
-	# 	      OneTB_ntfs_Success=1; 
-	# 	  else
-	# 	      echo "OneTB_ntfs-disken är ej monterad, synkar ej denna."
-	# 	      OneTB_ntfs_Success=0;
-	# 	  fi
-		  
-		  break;;
- 		     
-    esac
-done
-
-echo "Bilder," `date`" till $enhet" >> ~/Backup_dates_disks.txt
-
+fi
 echo "=========================================================="
 echo " Sammanställning av synk av följande enheter enligt nedan:"
 echo 
-echo " $Source till $NgNas: $Nas_Success"
-#echo " $Source till $OneTB_ntfs: $OneTB_ntfs_Success"
+echo " $Source till $Target: $Nas_Success"
 echo "=========================================================="

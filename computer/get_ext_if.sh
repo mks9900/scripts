d8d491fde3d7b3d3bc0c7db5e44f50af3d729a71
Johan Thor <johan.thor@sodra.com>
2021-04-29 21:23:21 +0200

    Cleanups.

--- computer/get_ext_if.sh
+++ computer/get_ext_if.sh
@@ -1,21 +1,16 @@
-#!/bin/bash
+#!/bin/zsh
 #
 # Namnger textfil:
-ipFile=$HOME/ext_ip.txt
+ipFile=$HOME/OneDrive/ext_ip.txt
 writtenIP="`cat $ipFile`"
 
 # Få tag i den externa adressen:
 checkedIP="`curl http://ipinfo.io/ip`"> /dev/null 2>&1
 echo $checkedIP
 
-# Jämför om nuvarande extern adress är samma som i textfilen.
-# Om de stämmer överens, gör inget. Om inte, så skickas ett mail mha mutt.
+# Jämför om nuvarande extern adress är samma som i textfilen:
 if [ "$writtenIP" =  "$checkedIP" ]; then
-    /usr/bin/notify-send $checkedIP
     exit 0
-    #echo "Programmet avslutas nu."
 else
     echo $checkedIP > $ipFile
-    /usr/bin/notify-send $checkedIP
-#    /usr/bin/mutt -s "External ip has changed" johan@birkagatan.com < $ipFile
 fi

fd6c9528446f5f66be9e9f7dfb08520a09b6b40c
Johan Thor <johanthor@icloud.com>
2022-12-14 15:00:41 +0100

    Added script for extracting and renaming Nikon Z 6-movies to disk

--- images/importera_z6-movies_rocky_ws.sh
+++ images/importera_z6-movies_rocky_ws.sh
@@ -1,11 +1,9 @@
 #!/bin/zsh
 
 Source=/media/johanthor/"NIKON Z 6"/DCIM/100NCZ_6/
-# /Volumes/"NIKON Z 6"/DCIM/100NCZ_6
-Target=/home/johanthor/tmp/import-z6-raw/
-# /Volumes/Raw_photos/raws/imports
-PhotoPath=/home/johanthor/Pictures/Raw/
-# /Volumes/Raw_photos/raws/
+Target=/home/johanthor/tmp/import-z6-raw/movies
+PhotoPath=/home/johanthor/hdd_storage/icloud_exif_photos
+
 
 clear
 
@@ -30,33 +28,29 @@ else
 
 	echo
 	echo "==================================================================================="
-	echo "Flyttar alla råfiler till målet:" #samt att vi kollar vad tiden är så vi kan räkna ut hur lång tid allt tog i slutet:
+	echo "Flyttar alla filmer till målet:" #samt att vi kollar vad tiden är så vi kan räkna ut hur lång tid allt tog i slutet:
 	echo "==================================================================================="
 
 	Amount="$(du -chsm $Source|awk '{print $1}'|head -1|sed -e 's/M//'|sed -e "s/\(\.[0-9]\).*/\1/g")"
-  #Amount="$(echo "$Amount2"|sed -e "s/\(\.[0-9]\).*/\1/g")"
-
 	Tstart="$(date +%s)"
 
-	cp -av "$Source"/*.NEF "$Target"
-	echo "\nNu är alla filer kopierade från minneskortet till flyttbara disken."
+	cp -av "$Source"/*.MOV "$Target"
+	echo "\nNu är alla filmer kopierade från minneskortet!"
 
 	#Felhantering om Ttrans=0!...
 	Ttrans="$(($(date +%s)-Tstart))"
 
 	cd "$Target"
 
-	Count="$(ls -1 *.NEF 2>/dev/null | wc -l)"
+	Count="$(ls -1 *.MOV 2>/dev/null | wc -l)"
 
 	echo
-	echo "Det finns $Count flyttade NEF-filer."
+	echo "Det finns $Count MOV-filer."
 	echo
 	echo
 	echo "==================================================================================="
-	echo "Döper om varje fil till något mer meningsfullt:"
+	echo "Döper om varje film till något mer meningsfullt:"
 	echo "==================================================================================="
-
-# Felhantering för *.NEF finns nedan:
 
 	if [ $Count = 0 ]
 	then
@@ -64,29 +58,31 @@ else
 	    exit; else
 
 	    Counter=1
-	    for RawFile in "$Target"/*.NEF; do
+	    for Movie in "$Target"/*.MOV; do
         # OBS! exiv2 ändrar format beroende på vilket språk terminalen har...
-        # Man kan alltså få ändra $x nedan:
-		FileDate="`exiv2 "$RawFile"|grep timestamp|awk '{print $4}'|sed -e 's/:/-/g'`"
-		FileTime="`exiv2 "$RawFile"|grep timestamp|awk '{print $5}'|sed -e 's/:/-/g'`" #|cut -c1-5`" #skär bort sekundrarna...
-		FocalLength="`exiv2 "$RawFile"|grep length|awk '{print $4}'|cut -d'.' -f1`"
-		ISOSpeed="`exiv2 "$RawFile"|grep "ISO"|awk '{print $4}'`"
-		Aperture="`exiv2 "$RawFile"|grep Aperture|awk '{print $3}'|grep F|sed -e s'/\./-/'|sed -e s'/F/f/'`" #Byt ut punkten mot _
-#		LensMaker="`exiv2 -pa "$RawFile"|grep "LensIDNumber"|awk '{print $4}'`"
-#		LensID="`exiv2 -pa "$RawFile"|grep "Exif.NikonLd3.LensIDNumber"|awk '{print $5"_"$6"-"$7}'`"
-		#CameraMaker="`exiv2 "$RawFile"|grep "make"|awk '{print $3}'|sed -e 's/:/-/g'`"
-		CameraModel="`exiv2 "$RawFile"|grep "model"|awk '{print $4,$5$6}'|sed -e 's/ /_/g'`"
-#		FileName=$FileDate"_kl-"$FileTime"_"$LensMaker"_"$FocalLength""mm_ISO$ISOSpeed"_"$Aperture
-		FileName=$FileDate"_kl"$FileTime"_"$CameraModel"_"$FocalLength"mm_"$Aperture"_ISO""$ISOSpeed"
+			# Man kan alltså få ändra $x nedan:
+			FileDate="`exiftool "$Movie"|grep "Create Date"|awk '{print $5}'|sed -e 's/:/-/g'|head -n 1`"
+			FileTime="`exiftool "$Movie"|grep "Create Date"|awk '{print $6}'|sed -e 's/:/-/g'|head -n 1`"
+			FocalLength="`exiftool "$Movie"|grep "Focal Length"|awk '{print $4}'|sed -e 's/:/-/g'|head -n 1`"
+			ISOSpeed="`exiftool "$Movie"|grep "ISO"|head -n 1|awk '{print $3}'`"
+			Aperture="`exiftool "$Movie"|grep "Aperture"|head -n 1|awk '{print $4}'|sed -e s'/\.//'`"
+			CameraModel="`exiftool "$Movie"|grep "Model"|head -n 1|awk '{print $3,$4,$5}'|sed -e 's/ /_/g'`"
 
-		if [ ! -d "$FileDate" ]; then
+			FileName=$FileDate"_kl"$FileTime"_"$CameraModel"_"$FocalLength"mm_f"$Aperture"_ISO""$ISOSpeed"
+
+			Year="`exiftool *.MOV|grep 'Create Date'|awk '{print $5}'|head -n1|cut -c1-4`"
+
+		if [ ! -d "$Year" ]; then
 		    Counter=1
-		    mkdir -p "$FileDate"
-		    mv -v "$RawFile" "$FileDate"/"$FileName""_"$Counter.nef
+			# echo "hej"
+			mkdir -p "$Year"/"$FileDate"
+			# mkdir -p $FileDate
+		    mv -v "$Movie" "$Year"/"$FileDate"/"$FileName""_"$Counter.mov
 		    Counter=$[Counter + 1]
+
 		else
-		    mv -v "$RawFile" "$FileDate"/"$FileName""_"$Counter.nef
-		    echo "$FileDate"/"$FileName""_"$Counter.nef
+			mv -v "$Movie" "$Year"/"$FileDate"/"$FileName""_"$Counter.mov
+		    # echo "$FileDate"/"$FileName""_"$Counter.mov
 		    Counter=$[Counter + 1]
 		fi
 
@@ -102,24 +98,32 @@ echo "==========================================================================
 echo "Filerna kopieras/flyttas till rätt kataloger i foto-strukturen, baserat på yyyy/yyyy-mm-dd."
 echo "==================================================================================="
 
-# Varför visas ej pwd nedan?
-# Kan även denna slås samman med den större for-loopen ovan?
+for YearDir in *; do
+    # echo $TempDir
+    # CurrentDirectory=$(pwd)
+    # Year="$(ls |sort|awk '{print $1}'|cut -c1-4|head -1)"
+	cd $YearDir
+	pwd
 
-for TempDir in *; do
-    echo $TempDir
-    CurrentDirectory=$(pwd)
-    Year="$(ls |sort|awk '{print $1}'|cut -c1-4|head -1)"
-    if [ ! -d "$PhotoPath"/"$Year"/"$TempDir" ]; then
-	echo "Katalogen finns ej: Flyttar hela katalogen..."
-	mv -vi "$TempDir"/ "$PhotoPath"/"$Year"
-	echo
-    else
-	echo "Katalogen finns: Kopierar filerna till katalogen..."
-	cp -ai "$TempDir"/*.nef "$PhotoPath"/"$Year"/"$TempDir"
-	echo "Städa manuellt undan bilderna i $TempDir!"
-	echo
+	for DateDir in *; do # == e.g. 2022-12-14
+		cd $DateDir
+		# ls
+		Year="`exiftool *.mov|grep 'Create Date'|awk '{print $5}'|head -n1|cut -c1-4`"
+		# echo "$Year"
+		pwd
+		cd ..
+		if [ ! -d "$PhotoPath"/"$Year"/"$DateDir" ]; then
+			echo "Katalogen finns ej: Flyttar hela katalogen..."
+			mv -vi "$DateDir"/ "$PhotoPath"/"$Year"
+			echo
+		else
+			echo "Katalogen finns: Kopierar filerna till katalogen..."
+			cp -ai "$DateDir"/*.mov "$PhotoPath"/"$Year"/"$DateDir"
+			echo "Städa manuellt undan bilderna i ~/tmp/$DateDir!"
+			echo
+		fi
+	done
 
-    fi
 done
 
 # Här kollas den sista tiden och subtraherar den första så att totalen fås:

0ea52310747342fc87c1712bc6e4e8785063bc03
Johan Thor <johanthor@icloud.com>
2022-12-29 15:25:17 +0100

    Changed backup script

--- backup/backup_bilder_rocky_ws.sh
+++ backup/backup_bilder_rocky_ws.sh
@@ -6,25 +6,26 @@
 # --delete tar bort filer vid målet som ej finns i källan.
 #
 
-Source="/home/johanthor/onedrive_privat/Pictures/"
+Source="/home/johanthor/Pictures/"
 
-Target="/home/johanthor/backup_nas/Pictures/"
+Target="/home/johanthor/hdd_storage/onedrive/Pictures/"
 
-mount_test="/home/johanthor/backup_nas/Pictures"
+# mount_test="/home/johanthor/backup_nas/Pictures"
 
-if [ -d "$mount_test" ]; then
-    echo "=========================================================="
-    echo "Speglar till ng-nas:"
-    echo "=========================================================="
-    /usr/bin/rsync -hlrtv --exclude 'lost+found' --exclude '.Trash-1000' --exclude '.DS_Store' --delete $Source $Target
-    Nas_Success=1;
-else
-    echo "Målenheten är inte monterad!"
-    Nas_Success=0    
+# if [ -d "$mount_test" ]; then
+echo "=========================================================="
+echo "Speglar till Onedrive:"
+echo "=========================================================="
+/usr/bin/rsync -hlrtv --exclude 'lost+found' --exclude '.Trash-1000' --exclude '.DS_Store' --delete $Source $Target
+# Nas_Success=1;
+# else
+# echo "Målenheten är inte monterad!"
+# Nas_Success=0
 
-fi
+# fi
 
 echo "=========================================================="
-echo " Sammanställning av synk av följande enheter enligt nedan:"
-echo " $Source till $Target: $Nas_Success"
+# echo " Sammanställning av synk av följande enheter enligt nedan:"
+# echo " $Source till $Target: $Nas_Success"
+echo "Klar!"
 echo "=========================================================="

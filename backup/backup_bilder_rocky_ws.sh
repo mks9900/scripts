3f35d775f55d9d37766a91c05554d74659db9f0d
Johan Thor <johanthor@icloud.com>
2023-01-04 21:24:22 +0100

    Changed backup script

--- backup/backup_bilder_rocky_ws.sh
+++ backup/backup_bilder_rocky_ws.sh
@@ -6,25 +6,25 @@
 # --delete tar bort filer vid målet som ej finns i källan.
 #
 
-Source="/home/johanthor/onedrive_privat/Pictures/"
+Target="/home/johanthor/hdd_storage/onedrive/Pictures/"
 
-Target="/home/johanthor/backup_nas/Pictures/"
+Source="/home/johanthor/Pictures/"
 
 mount_test="/home/johanthor/backup_nas/Pictures"
 
-if [ -d "$mount_test" ]; then
-    echo "=========================================================="
-    echo "Speglar till ng-nas:"
-    echo "=========================================================="
-    /usr/bin/rsync -hlrtv --exclude 'lost+found' --exclude '.Trash-1000' --exclude '.DS_Store' --delete $Source $Target
-    Nas_Success=1;
-else
-    echo "Målenheten är inte monterad!"
-    Nas_Success=0    
+# if [ -d "$mount_test" ];
+echo "=========================================================="
+echo "Speglar till Onedrive"
+echo "=========================================================="
+/usr/bin/rsync -hlrtv --exclude 'lost+found' --exclude '.Trash-1000' --exclude '.DS_Store' --delete $Source $Target
+Success=1;
+# else
+# echo "Målenheten är inte monterad!"
+# Success=0
 
-fi
+# fi
 
 echo "=========================================================="
 echo " Sammanställning av synk av följande enheter enligt nedan:"
-echo " $Source till $Target: $Nas_Success"
+echo " $Source till $Target: $Success"
 echo "=========================================================="
